0eeec6d180a954d53fa4b41a7022f1897f05d7d1
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-11 10:53:44 +0200

    Arrray CopyArray

--- src/main/java/array/CopyArray.java
+++ src/main/java/array/CopyArray.java
@@ -1,10 +1,23 @@
 package array;
 
+import java.util.Arrays;
+
 public class CopyArray {
-    public static double[] copyArray(double[] src){
-        double[] dist  = new double[src.length];
-        System.arraycopy(src, 0, dist,0, src.length);
-        return dist;
+
+    public static double[] copyArray(double[] v){
+        double[] s = new double[v.length];
+        System.arraycopy(v,0,s,0,v.length);
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+        double[] s;
+        s = new double[]{1, 2, 3, 4};
+
+        System.out.println("arr: "+ Arrays.toString(copyArray(s)));
+
     }
 
 }
+
