7a11e49e55508a7a3cc35f01d74f3496208afea3
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-18 11:59:20 +0200

    functional ToString

---
+++ src/esercizi/java/funcional/ToString.java
@@ -0,0 +1,14 @@
+package funcional;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ToString {
+
+    public static <T> String toString(Stream<T> stream, int n){
+        return stream.limit(n).map(Objects::toString).collect(Collectors.joining(","));
+    }
+
+}
