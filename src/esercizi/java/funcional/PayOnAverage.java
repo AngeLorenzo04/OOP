7cc81368aab6a506606e6f63d0102eae67937488
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-18 11:58:54 +0200

    functional PayOnAverage

---
+++ src/esercizi/java/funcional/PayOnAverage.java
@@ -0,0 +1,16 @@
+package funcional;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PayOnAverage {
+
+    public static Map<String, Double> payOnAverage(Map<String, Double> fees){
+        return fees.entrySet().stream().collect(Collectors.groupingBy(
+                e -> e.getKey().substring(0,3),
+                Collectors.averagingDouble(Map.Entry::getValue
+                )
+        ));
+    }
+
+}
