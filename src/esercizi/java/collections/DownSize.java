ade920f2f65239c7c9d127b935a0c3997ffaf07e
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-21 18:19:54 +0200

    Collections bis DownSize

--- src/esercizi/java/collections/DownSize.java
+++ src/esercizi/java/collections/DownSize.java
@@ -4,17 +4,19 @@ import java.util.Iterator;
 import java.util.List;
 
 public class DownSize {
-    public static void downsize(List<String> list, int n) {
 
-        int i = 1;
-        Iterator<String> it = list.iterator();
-        while (it.hasNext()){
-            it.next();
-            if( i % n == 0){
-                it.remove();
+
+    public static void downsize(List<String> list, int n){
+
+        int index = 1;
+        for(Iterator<String> iterator = list.iterator(); iterator.hasNext();){
+            iterator.next();
+            if(index++ % n == 0){
+                iterator.remove();
             }
-            i++;
         }
 
     }
+
+
 }
