0c409118421937f6a8d11a8a802371416dea8776
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-21 18:23:58 +0200

    oop BankAccount bis

--- src/esercizi/java/oop/bankaccount/BankAccountPro.java
+++ src/esercizi/java/oop/bankaccount/BankAccountPro.java
@@ -1,42 +1,20 @@
 package oop.bankaccount;
 
-public class BankAccountPro extends AbstractBankAccount{
+public class BankAccountPro extends AbstractBankAccount {
 
-
-    /**
-     * Construct a new BankAccount
-     *
-     * @param IBAN         the IBAN of the account
-     * @param balance      the initial balance of the account
-     * @param operationFee the fee to be applied to deposit and withdraw operations
-     * @param interestRate the interest rate to be applied
-     */
-    protected BankAccountPro(String IBAN, double balance, double operationFee, double interestRate) {
-        super(IBAN, balance, 0.0, interestRate);
+    protected BankAccountPro(String IBAN, double balance) {
+        super(IBAN, balance, 0.2, 1);
     }
-
-    /**
-     * Deposit an amount on the account. Fees are applied.
-     * @param amount the amount to be deposited
-     */
     @Override
     public void deposit(double amount){
-        super.deposit(amount);
         applyFee();
+        this.balance += amount;
     }
 
-    /**
-     * Withdraw an amount from the account. Fees are applied.
-     * @param amount the amount to be withdrawn
-     * @return the amount withdrawn
-     */
-    @Override
     public double withdraw(double amount){
         applyFee();
-        return super.withdraw(amount);
+        this.balance -= amount;
+        return amount;
     }
-
-
-
 
 }
