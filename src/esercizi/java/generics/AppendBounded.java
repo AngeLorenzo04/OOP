7186e371ecae9d1b1342e03d82b96e0046650e76
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-21 18:20:21 +0200

    Generics bis AppendBounded

--- src/esercizi/java/generics/AppendBounded.java
+++ src/esercizi/java/generics/AppendBounded.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class AppendBounded {
 
-    public static <T, S extends T> void append(List<T> dst, List<S> src) {
-
-            dst.addAll(src);
+    public static <T> void append(List<T> dst, List<  ? extends T> src){
+        dst.addAll(src);
     }
+
 }
