123a737afbce4ea62d8ab14c13d839673161ab61
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-21 18:21:24 +0200

    Generics bis Measure

---
+++ src/esercizi/java/generics/Measure.java
@@ -0,0 +1,35 @@
+package generics;
+
+public class Measure {
+
+    public interface Measurer<T> {
+        double measure(T obj);
+    }
+
+
+    public static <T> T max(T[] array, Measurer<T> measurer){
+
+        T max = array[0];
+        for(T e : array){
+
+            if(measurer.measure(e) > measurer.measure(max)){
+                max = e;
+            }
+
+        }
+        return max;
+
+    }
+    public static <T> T min(T[] array, Measurer<T> measurer){
+        T min = array[0];
+        for(T e : array){
+
+            if(measurer.measure(e) < measurer.measure(min)){
+                min = e;
+            }
+
+        }
+        return min;
+    }
+
+}
