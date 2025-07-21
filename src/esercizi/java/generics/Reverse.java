50f78482073a40e521d67bc0998b2b90676b840b
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-21 18:22:07 +0200

    Generics bis Reverse

--- src/esercizi/java/generics/Reverse.java
+++ src/esercizi/java/generics/Reverse.java
@@ -6,19 +6,27 @@ public class Reverse {
 
     public static <T> void reverse(List<T> list){
 
-        for(int i = 0 ; i < Math.floor((double) list.size() / 2); i++){
-            T e  = list.get(list.size() - i - 1);
-            T tmp  = list.get(i);
-            list.set(i,e);
-            list.set(list.size() - i - 1, tmp);
+        for(int i = 0; i < Math.floor((double) list.size() /2); i++){
+
+            T tmp = list.get(i);
+            list.set(i,list.get(list.size() - i - 1));
+            list.set(list.size() - i -1,tmp);
+
         }
+
 
     }
 
-    /*
-        [1,2,3,4,5]
-        [5,2,3,4,1]
-        [5,4,3,2,1]
+
+    /**
+     *
+     * [1,2,3,4]
+     * [4,2,3,1]
+     * [4,3,2,1]
+     *
+     * [1,2,3]
+     * [3,2,1]
+     *
      */
 
 }
