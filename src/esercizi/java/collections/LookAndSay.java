ab2df42025a055fcf67e647781976a75f384c423
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-21 18:20:03 +0200

    Collections bis LookAndSay

--- src/esercizi/java/collections/LookAndSay.java
+++ src/esercizi/java/collections/LookAndSay.java
@@ -2,38 +2,30 @@ package collections;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class LookAndSay {
-
-    public static List<Long> lookAndSay(long n){
-
-        if(n < 1L){
+    public static List<Long> lookAndSay(long n) {
+        if (n < 1) {
             return new ArrayList<>();
         }
 
-        List<Long> sequenza = new ArrayList<>();
-
-        sequenza.add(1L);
-
-        for(int i = 1; i < n ; i++){
-
-            String prev = sequenza.get(i-1).toString();
+        List<Long> sequence = new ArrayList<>();
+        sequence.add(1L);
+        for (int i = 1; i < n; i++) {
+            String previous = sequence.get(i - 1).toString();
             StringBuilder next = new StringBuilder();
             int count = 1;
-            for( int j = 1; j < prev.length(); j++){
-                if(prev.charAt(j) == prev.charAt(j - 1)){
+            for (int j = 1; j < previous.length(); j++) {
+                if (previous.charAt(j) == previous.charAt(j - 1)) {
                     count++;
-                }else{
-                    next.append(count).append(prev.charAt(j-1));
+                } else {
+                    next.append(count).append(previous.charAt(j - 1));
                     count = 1;
                 }
             }
-            next.append(count).append(prev.charAt(prev.length() - 1));
-            sequenza.add(Long.parseLong(next.toString()));
+            next.append(count).append(previous.charAt(previous.length() - 1));
+            sequence.add(Long.parseLong(next.toString()));
         }
-
-        return sequenza;
+        return sequence;
     }
-
 }
