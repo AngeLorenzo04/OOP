93444df689e37bcc1e675c7aa7faa5556a47e060
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2025-07-21 18:20:16 +0200

    Generics bis Append

--- src/esercizi/java/generics/Append.java
+++ src/esercizi/java/generics/Append.java
@@ -6,10 +6,6 @@ public class Append {
 
     public static <T> void append(List<T> dst, List<T> src){
 
-        if(src.isEmpty()){
-            return;
-        }
-
         dst.addAll(src);
 
     }
